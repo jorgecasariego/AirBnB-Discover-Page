1889d22f07e0ad03383a433ebcc6c17706c77d44
Jorge Casariego <jorgekasa@gmail.com>
2015-08-17 20:24:22 -0400

    Pasos finales para agregar bar button item y cambiar el tipo de letra

--- AirBnBProject/PlacesTableViewController.swift
+++ AirBnBProject/PlacesTableViewController.swift
@@ -15,11 +15,7 @@ class PlacesTableViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+        self.tableView.separatorStyle = UITableViewCellSeparatorStyle.None
     }
 
     override func didReceiveMemoryWarning() {
